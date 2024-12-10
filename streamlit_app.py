8e3411606873dc09de8557c7e0d4766573460dbd
Ayu Fitriani <74908927+Ayu-Fitriani131@users.noreply.github.com>
2024-12-10 14:00:34 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,6 @@ import streamlit as st
 st.title('🤖 Machine Learning App')
 
 st.info('This is app builds a machine learning model')
+
+df = pd.read_csv('https://raw.githubusercontent.com/Ayu-Fitriani131/flu_vaccination/refs/heads/main/training_set_features.csv')
+df
