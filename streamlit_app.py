15707c61ce727140bf754c58de4c2a4a398f8c91
Ayu Fitriani <74908927+Ayu-Fitriani131@users.noreply.github.com>
2024-12-13 11:09:31 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,11 +4,3 @@ import pandas as pd
 st.title('🤖 Machine Learning App')
 
 st.info('This app builds a machine learning model')
-
-# Corrected Google Drive link
-csv_url = 'https://drive.google.com/drive/u/0/folders/16KqGaTACyiqZSn52qYlLIIJ8lpJTzeZP'  # Replace FILE_ID with the actual file ID
-try:
-    df = pd.read_csv(csv_url)
-    st.dataframe(df)
-except Exception as e:
-    st.error(f"Error loading data: {e}")
