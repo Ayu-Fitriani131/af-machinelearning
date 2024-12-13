6ed2c4554e991b962bdec5308f687cc1265aeac2
Ayu Fitriani <74908927+Ayu-Fitriani131@users.noreply.github.com>
2024-12-13 09:53:04 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,5 @@ st.title('🤖 Machine Learning App')
 
 st.info('This is app builds a machine learning model')
 
-df = pd.read_csv('https://www.kaggle.com/datasets/darkknight98/flu-shot-prediction')
+df = pd.read_csv('https://drive.google.com/drive/u/0/folders/16KqGaTACyiqZSn52qYlLIIJ8lpJTzeZP')
 df
